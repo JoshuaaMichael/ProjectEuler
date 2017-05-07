53cdc9e6286c3c7f2ac16e15aae0eba80ef41cf7
Joshua Cameron <josh@usr.name>
2017-05-07 20:36:23 +1000

    Solved problem 11

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -7,7 +7,7 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            Problem problem = new Problem10();
+            Problem problem = new Problem11();
             problem.Setup();
             problem.Run();
             problem.Cleanup();
