056629a962c82c82b3de590e1151157318318cac
Joshua Michael <josh@usr.name>
2017-08-03 21:57:50 +1000

    Solved project 20

---
+++ ProjectEuler/Problems/Problem20.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ProjectEulerLibrary;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace ProjectEuler.Problems
+{
+	class Problem20 : Problem
+	{
+		public override void Run()
+		{
+            BigInteger factorialValue = 100;
+
+            for(int i = 100; i > 0; i--)
+            {
+                factorialValue *= i;
+            }
+
+            Console.WriteLine("Result is: {0}", factorialValue);
+
+            int sumDigitValue = 0;
+            string powerTwoValueString = factorialValue.ToString();
+
+            for(int i = 0; i < powerTwoValueString.Length; i++)
+            {
+                sumDigitValue += int.Parse(powerTwoValueString[i] + "");
+            }
+
+			Console.WriteLine("Result is: {0}", sumDigitValue);
+		}
+	}
+}
