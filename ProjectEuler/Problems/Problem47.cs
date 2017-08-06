be8dac8daf9df7d6143b1b0c00d2cb5a3a0bfb0d
Joshua Michael <josh@usr.name>
2017-08-06 22:21:38 +1000

    Solved problem 47

---
+++ ProjectEuler/Problems/Problem47.cs
@@ -0,0 +1,30 @@
+﻿using ProjectEulerLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace ProjectEuler.Problems
+{
+	class Problem47 : Problem
+	{
+        public override void Run()
+        {
+            List<long> preComputedPrimes = MathHelper.ListOfPrimes(10000);
+            int lastConsecutive = 0;
+            int consecutive = 0;
+            for (int i = 16; consecutive < 4; i++)
+            {
+                if(MathHelper.CountDistinctPrimeFactors(preComputedPrimes, i) == 4)
+                {
+                    lastConsecutive = i;
+                    consecutive += 1;
+                }
+                else
+                {
+                    consecutive = 0;
+                }
+            }
+
+            Console.WriteLine("Result is: {0}, {1}, {2}, {3}", lastConsecutive - 3, lastConsecutive - 2, lastConsecutive - 1, lastConsecutive);
+        }
+    }
+}
