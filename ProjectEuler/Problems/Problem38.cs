1f0d3258634a109c422459950da5d12831eb83a5
Joshua Michael <josh@usr.name>
2017-08-07 01:04:18 +1000

    Solved problem 38

---
+++ ProjectEuler/Problems/Problem38.cs
@@ -0,0 +1,32 @@
+﻿using ProjectEulerLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace ProjectEuler.Problems
+{
+	class Problem38 : Problem
+	{
+        public override void Run()
+        {
+            long max = 0;
+            for(int a = 1; a < 9876; a++)
+            {
+                string str = "";
+                for(int b = 1; str.Length < 9; b++)
+                {
+                    str += (a * b);
+                }
+                if(str.Length == 9 && long.Parse(str) > max)
+                {
+                    if(MathHelper.IsPandigital(str))
+                    {
+                        max = long.Parse(str);
+                    }                  
+                }
+            }
+
+            Console.WriteLine("Result is: {0}", max);
+        }
+
+    }
+}
