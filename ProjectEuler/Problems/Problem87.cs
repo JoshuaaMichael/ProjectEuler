42813392e17cc25ef39838bf8f373890f01ddba3
Joshua Michael <josh+9m879qbn@usr.name>
2017-08-16 21:36:20 +1000

    Removing old comments

--- ProjectEuler/Problems/Problem87.cs
+++ ProjectEuler/Problems/Problem87.cs
@@ -8,17 +8,6 @@ namespace ProjectEuler.Problems
 	{
 		public override void Run()
 		{
-            //The upper bound of the fourth power is primes that go to 83
-            //The upper bound of the third power is primes that go to 367
-            //The upper bound of the second power is primes that go to 7069
-            //make 3 lists, one of each that go to that max
-            //make 3 more lists, one for each second, third and fourth power
-            //int total = 0;
-            //for 4th power
-            //for 3rd power (3rd power + 4th power < num)
-            //for 2nd power (2nd power + 3rd power + 4th power < num)
-            //if () total +=
-
             int[] primes = PrimeHelper.ArrayOfPrimeNumbersToMax(7069 + 1);
 
             List<int> secondPoweredPrimes = new List<int>();
