4736bc3eca47bcb88366988b9fb3e950619a9cf3
Joshua Michael <josh@usr.name>
2017-08-27 01:37:32 +1000

    Solved problem 76

---
+++ ProjectEuler/Problems/Problem76.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ProjectEulerLibrary;
+using System.Collections.Generic;
+
+namespace ProjectEuler.Problems
+{
+	class Problem76 : Problem
+	{
+		public override void Run()
+		{
+            int countOfWays = 0;
+
+            RecursiveSummationCounter(ref countOfWays, 100);
+            countOfWays--;
+
+            Console.WriteLine("Result is: {0}", countOfWays);
+		}
+
+        private void RecursiveSummationCounter(ref int countOfWays, int remainder, int minimum = 1)
+        {
+            if (remainder == 0)
+            {
+                countOfWays++;
+                return;
+            }
+
+            for(int i = minimum; i <= remainder; i++)
+            {
+                RecursiveSummationCounter(ref countOfWays, remainder - i, i);
+            }
+        }
+	}
+}
