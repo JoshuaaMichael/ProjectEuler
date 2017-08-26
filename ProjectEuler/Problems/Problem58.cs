76ab1e81620def00b40f3726c7c1a098f6424eb9
Joshua Michael <josh@usr.name>
2017-08-27 01:37:20 +1000

    Solved problem 63

---
+++ ProjectEuler/Problems/Problem58.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ProjectEulerLibrary;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace ProjectEuler.Problems
+{
+    class Problem58 : Problem
+    {
+        public override void Run()
+        {
+            int totalDiagonalNumbers = 1;
+            int totalPrimes = 0;
+
+            for (int i = 3; ; i += 2)
+            {
+                totalDiagonalNumbers += 4;
+                for (int j = 0; j < 4; j++)
+                {
+                    if(MathHelper.IsPrime((i * i) - j * (i-1)))
+                    {
+                        totalPrimes += 1;
+                    }
+                }
+
+                if((double)totalPrimes / (double)totalDiagonalNumbers < 0.1)
+                {
+                    Console.WriteLine("Result: {0}", i);
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ ProjectEuler/Problems/Problem63.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using ProjectEulerLibrary;
+
+namespace ProjectEuler.Problems
+{
+	class Problem63 : Problem
+	{
+        public override void Run()
+        {
+            //  10^(n-1) <= a^n < 10^n restricts a^n to exactly n digits
+            //  It follows, from WolframAlpha solving of equations:
+            //  1 <= a <= 9
+            //  1 <= n <= 22
+
+            int count = 0;
+
+            for(int a = 1; a <= 9; a++)
+            {
+                for (int n = 1; n <= 22; n++)
+                {
+                    BigInteger value = BigInteger.Pow(a, n);
+                    if(value.ToString().Length == n)
+                    {
+                        count += 1;
+                    }
+                }
+            }
+
+            Console.WriteLine("Result is: {0}", count);
+        }
+    }
+}
