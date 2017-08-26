9c73ddf82e5a7f16774bae9cbdefd7dd490989d1
Joshua Michael <josh@usr.name>
2017-08-27 01:37:41 +1000

    Solved problem 77

---
+++ ProjectEuler/Problems/Problem77.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ProjectEulerLibrary;
+
+namespace ProjectEuler.Problems
+{
+    class Problem77 : Problem
+	{
+        public override void Run()
+        {
+            int[] primes = PrimeHelper.ArrayOfPrimeNumbersToMax(5000);
+            
+            for (int i = 10; ; i++)
+            {
+                Console.Write("Current i - {0}, ", i);
+                int countOfWays = 0;
+                RecursiveSummationCounter(ref countOfWays, i, primes);
+                Console.WriteLine("count of ways - {0}", countOfWays);
+                if (countOfWays > 5000)
+                {
+                    Console.WriteLine("Result - {0}", i);
+                    return;
+                }
+            }
+        }
+
+        private void RecursiveSummationCounter(ref int countOfWays, int remainder, int[] primes, int minimum = 0)
+        {
+            if (remainder == 0)
+            {
+                countOfWays++;
+                return;
+            }
+
+            for (int i = minimum; primes[i] <= remainder; i++)
+            {
+                RecursiveSummationCounter(ref countOfWays, remainder - primes[i], primes, i);
+            }
+        }
+    }
+}
