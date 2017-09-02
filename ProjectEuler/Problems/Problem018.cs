90ffc5c039c17626147aabc69c44bae9b3f2ba95
Joshua Michael <josh@usr.name>
2017-09-02 15:08:15 +1000

    Solved problem 18

--- ProjectEuler/Problems/Problem018.cs
+++ ProjectEuler/Problems/Problem018.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using ProjectEulerLibrary;
+using System.IO;
+using System.Collections.Generic;
 
 namespace ProjectEuler.Problems
 {
@@ -7,16 +9,43 @@ namespace ProjectEuler.Problems
     {
         public override void Run()
         {
-            int sum = 0;
-            for(int i = 1; i < 1000; i++)
+            /*
+               3
+              7 4
+             2 4 6
+            8 5 9 3
+            */
+
+            List<List<int>> triangle = ReadTriangle("./Resources/p018_triangle.txt");
+
+            for(int i = triangle.Count - 2; i >= 0; i--) //Start at second last line, then go up, first line will be result
             {
-                if(MathHelper.IsMultiple(i, 3) || MathHelper.IsMultiple(i, 5))
+                for(int j = 0; j < triangle[i].Count; j++) //For each element in line, calculate which is max path from below
                 {
-                    sum += i;
+                    triangle[i][j] += Math.Max(triangle[i+1][j], triangle[i+1][j+1]);
                 }
             }
 
-            Console.WriteLine("Result is: {0}", sum);
+            Console.WriteLine("Result is: {0}", triangle[0][0]);
+        }
+
+        public static List<List<int>> ReadTriangle(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename);
+
+            List<List<int>> triangle = new List<List<int>>();
+
+            foreach (string line in lines)
+            {
+                List<int> temp = new List<int>();
+                string[] numbers = line.Trim().Split(' ');
+                foreach (string num in numbers)
+                {
+                    temp.Add(int.Parse(num));
+                }
+                triangle.Add(temp);
+            }
+            return triangle;
         }
     }
 }
